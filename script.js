ed28d6b536c612522757fe3a19a5243c7beb3939
Arsientii Stupak <haskeryt322009@gmail.com>
2026-04-09 16:43:55 +0200

    2 сщььше

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const input = document.querySelector("#city");
-const result = document.querySelector("#result");
 const btn = document.querySelector("#btn");
+const cityName = document.querySelector("#city-name");
+const temperature = document.querySelector("#temp");
+const wind = document.querySelector("#wind");
+const pressure = document.querySelector("#weather");
+const weatherIcon = document.querySelector("#weather-icon");
 
 btn.addEventListener("click", getWeather);
 
 input.addEventListener("keydown", (e) => {
     if (e.key === "Enter") getWeather();
 });
+
+
 
 
 async function getWeather() {
@@ -17,27 +23,40 @@ async function getWeather() {
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-
+                cityName.textContent = "loading...";
+            temperature.textContent = "0°C";
+            wind.textContent = "loading..."
+            weather.textContent = "loading..."
+            weatherIcon.src = "";
+            weatherIcon.alt = "";
     try {
-        result.textContent = "Загрузка...";
+
         let res = await fetch(url)
 
         let data = await res.json();
         if (data.cod !== 200) {
-            result.textContent = "Город не найден ❌";
+            cityName.textContent = "City not found";
+            temperature.textContent = "0°C"
+            wind.textContent = "wind"
+            weather.textContent = "weather"
+            weatherIcon.src = "./img/1d7ed459a70823cb1f56251769a0ad58.jpg";
+            weatherIcon.alt = "";
             return;
         }
         console.log(data);
 
-result.textContent = `
-Город: ${data.name}
-Температура: ${data.main.temp}°C
-Погода: ${data.weather[0].description}
-`;
+
+        cityName.textContent = data.name;
+        temperature.textContent = Math.floor(data.main.temp) + "°C";
+        wind.textContent = "wind" + Math.floor(data.wind.speed) + " м/с";
+        weather.textContent = "weather: " + data.weather[0].description;
+        weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        weatherIcon.alt = data.weather[0].description;
 
     } catch (error) {
-        result.textContent = "Ошибка загрузки данных ❌";
+        result.textContent = "Error loading data(";
     }
+
 }
 
 
